5c6cda9daca99428f9ff2b695851ec075cb50fcb
Kjubism <dev@hellothere.de>
2022-06-08 13:51:32 +0200

    fix await promise when getting data from raspberry

--- src/services/getRaspberry.js
+++ src/services/getRaspberry.js
@@ -1,23 +1,17 @@
-const raspiInfo = require('../../node_modules/raspberry-info');
+const raspiInfo = require('raspberry-info');
 
 class getRaspberry {
 	constructor(res, req) {
 		this.res = res;
 		this.req = req;
 	}
-	getData() {
+	async getData() {
 		const data = {
-			time: raspiInfo.getCurrentTimeAndHost().then(output => { return output; }),
-			GPU: raspiInfo.getGPUTemperature().then(output => { return output; }),
-			CPU: raspiInfo.getCPUTemperature().then(output => { return output; }),
-			RAM: raspiInfo.getMemoryFree().then(output => { return output; })
+			time: await raspiInfo.getCurrentTimeAndHost(),
+			GPU: await raspiInfo.getGPUTemperature(),
+			CPU: await raspiInfo.getCPUTemperature(),
+			RAM: await raspiInfo.getMemoryUsage()
 		}
-		/*const data = {
-			time: 20,
-			GPU: '23°C',
-			CPU: '64°C',
-			RAM: '300mb'
-		}*/
 		this.res.render('index', { data });
 
 	}
